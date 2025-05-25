c0173123eac16b39e87043481ddc1c3a03d50265
mayank-58k <mayankbamne6@gmail.com>
2025-05-25 12:08:58 +0530

    Update GuessNumberGame.java
    This is a simple console-based number guessing game built in Java.
    The program generates a random number between 0 and 9 and keeps asking the user to guess it.
    If the guess is correct, the program congratulates the user and ends.
    Otherwise, it keeps asking until the correct number is guessed.

--- GuessNumberGame.java
+++ GuessNumberGame.java
@@ -1,1 +1,21 @@
 
+import java.util.*;
+public class GuessNumberGame {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Welcome");
+        int gues = (int)(Math.random()*10);
+        while (true) {
+            System.out.print("Enter the Guess : ");
+            int n = sc.nextInt();
+            if (n==gues) {
+                System.out.println("correct guess ! ");
+                System.out.println("congrast ");
+                break;
+            } 
+            else {
+                System.out.println("Try Again ");
+            }
+        }
+    }
+}
